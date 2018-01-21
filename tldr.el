30290e19bbca1b90cb5d934eaf00bf938800bc33
showgood <showgood21@gmail.com>
2018-01-20 22:55:53 -0500

    fix rename directory error when first time running or update doc

--- tldr.el
+++ tldr.el
@@ -144,7 +144,7 @@
         (url-copy-file tldr-source-zip-url tldr-saved-zip-path 'overwrite)
         (shell-command (format "unzip -d %s %s" (file-truename user-emacs-directory) tldr-saved-zip-path))
         (delete-file tldr-saved-zip-path)
-        (rename-file (concat (file-truename user-emacs-directory) "tldr-master") tldr-directory-path)
+        (shell-command (format "mv %s %s" (concat (file-truename user-emacs-directory) "tldr-master") tldr-directory-path))
         (message "Now tldr docs is updated!"))))
 
 
